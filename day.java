f1d0239107631b1a5d7fba85fbee965bdc0c4adb
RohitReddy16 <rohitpreddi@gmail.com>
2021-09-06 07:33:51 +0530

    first

--- day.java
+++ day.java
@@ -1,5 +1,9 @@
 public class day {
     public static void main(String[] args){
         System.out.println("Hi");
+        System.out.println("How are you?");
+        int a=3,b=5;
+        int sum=a+b;
+        System.out.println(sum);
     }
 }
